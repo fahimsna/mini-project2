1515788d1e2a9933b3115365c77235fd519ddd19
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-09 12:46:20 +0600

    Add database to the api

--- authApi/controllers/userController.js
+++ authApi/controllers/userController.js
@@ -1,10 +1,13 @@
-let registerUser = async(req, res) => {
+let User = require("../models/User");
+let registerUser = async (req, res) => {
   let data = req.body;
+  let user=new User(data);
+  let result=await user.save();
 
   res.send({
     status: 1,
     message: "Data Inserted Successfully",
-    data,
+    result,
   });
 };
 
