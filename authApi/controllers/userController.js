497846d6e1a32b59c9509ed7f67383ce933168ea
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-09 13:05:52 +0600

    Add post api try catch

--- authApi/controllers/userController.js
+++ authApi/controllers/userController.js
@@ -1,16 +1,33 @@
 let User = require("../models/User");
 let bcrypt = require("bcrypt");
 let registerUser = async (req, res) => {
-  let data = req.body;
-  data.password = await bcrypt.hash(data.password, 10);
-  let user = new User(data);
-  let result = await user.save();
+  try {
+    let data = req.body;
+    let existingUser = await User.findOne({
+      email: data.email,
+    });
+    if (existingUser) {
+      return res.status(403).json({
+        status: 0,
+        message: "Email Already Exists",
+      });
+    }
+    data.password = await bcrypt.hash(data.password, 10);
+    let user = new User(data);
+    let result = await user.save();
 
-  res.send({
-    status: 1,
-    message: "Data Inserted Successfully",
-    result,
-  });
+    res.send({
+      status: 1,
+      message: "Data Inserted Successfully",
+      result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 0,
+      message: "Failed to register user",
+      error,
+    });
+  }
 };
 
 module.exports = registerUser;
