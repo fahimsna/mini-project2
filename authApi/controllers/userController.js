dddbc5bf9ac9fd3923cff12f5576a2c676034ec5
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-09 12:50:33 +0600

    Add password protection

--- authApi/controllers/userController.js
+++ authApi/controllers/userController.js
@@ -1,8 +1,10 @@
 let User = require("../models/User");
+let bcrypt = require("bcrypt");
 let registerUser = async (req, res) => {
   let data = req.body;
-  let user=new User(data);
-  let result=await user.save();
+  data.password = await bcrypt.hash(data.password, 10);
+  let user = new User(data);
+  let result = await user.save();
 
   res.send({
     status: 1,
